79bbaf326a4e2f3b8b4b77dcff01536c6107ebb7
nhefner12 <nick.g.hefner@gmail.com>
2019-11-10 00:20:44 -0800

    page routing and some UI spikes

--- src/front-end/app.py
+++ src/front-end/app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import os
 app = Flask(__name__)
 
 @app.route('/')
+def Index():
+    return redirect(url_for('Model'))
+
+@app.route('/model')
 def Model():
     return render_template('ScoreAssesment.html')
 
 
-@app.route('/Visualization')
+@app.route('/visualization')
 def Visualization():
     return render_template('DataVisualization.html')
+
+@app.route('/about')
+def About():
+    return render_template('About.html')
 
 
 if __name__ == '__main__':
