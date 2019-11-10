320656de0e64a7a92252c34f663d56b6c940a172
nhefner12 <nick.g.hefner@gmail.com>
2019-11-10 01:50:25 -0800

    initial form and API call implementd

--- src/front-end/app.py
+++ src/front-end/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 import os
 app = Flask(__name__)
 
@@ -8,16 +8,24 @@ def Index():
 
 @app.route('/model')
 def Model():
-    return render_template('ScoreAssesment.html')
+    return render_template('ScoreAssesment.html', active_page="model")
 
 
 @app.route('/visualization')
 def Visualization():
-    return render_template('DataVisualization.html')
+    return render_template('DataVisualization.html', active_page="visualization")
 
 @app.route('/about')
 def About():
-    return render_template('About.html')
+    return render_template('About.html', active_page="about")
+
+# API calls
+@app.route('/generateModel', methods=['POST'])
+def GenerateModel():
+    name = request.form['field1']
+    return name
+    # your code
+    # return a response
 
 
 if __name__ == '__main__':
