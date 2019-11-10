8f9da26a92f683867c3185a77c01059e68b37ccf
Code n Code <sadafmehds@gmail.com>
2019-11-10 13:28:22 -0800

    working model

--- src/front-end/app.py
+++ src/front-end/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
+from onehotlist import onehotnames
 import pandas as pd
 import pickle
 import os
@@ -28,16 +29,41 @@ def About():
 @app.route('/generateModel', methods=['POST'])
 def GenerateModel():
     values = request.form.to_dict()
-<<<<<<< HEAD
     df = pd.DataFrame(values, index=[0])
-    df_onehot=pd.get_dummies(df,columns=['day_bin','pdays_bin','job','marital','education','default','housing','loan','contact','poutcome'])
-    prediction = model.predict_proba(df_onehot.values)
-=======
-    print(values)
-    return 'hi'
->>>>>>> 92753c57016f896b45888e81ce09803a07dd7816
 
-    return render_template("ScoreAssessment.html", prediction = prediction)
+    df['age'] = df['age'].astype('int')
+    df['balance'] = df['balance'].astype('int')
+    df['month'] = df['month'].astype('int')
+    df['duration'] = df['duration'].astype('int')
+    df['campaign'] = df['campaign'].astype('int')
+    df['previous'] = df['previous'].astype('int')
+
+    df['day_bin'] = df['day_bin'].astype('category')
+    df['pdays_bin'] = df['pdays_bin'].astype('category')
+
+    df_onehot=pd.get_dummies(df,columns=['day_bin','pdays_bin','job','marital','education','default','housing','loan','contact','poutcome'])
+
+    headers = onehotnames()
+    for i in headers:
+        if i in df_onehot.columns:
+            pass
+        else:
+            df_onehot[i]=0
+    
+    # get=[]
+    # for i in df_onehot.columns:
+    #     get.append(i)
+    #     print("'{}',".format(i))
+    # print('='*50)
+    # mod=[]
+    # for i in headers:
+    #     mod.append(i)
+    #     print("'{}',".format(i))
+    #     print(i)
+
+    prediction = model.predict_proba(df_onehot.values)
+
+    return render_template("ScoreAssesment.html", prediction = round(prediction,2))
 
 if __name__ == '__main__':
     app.run()
